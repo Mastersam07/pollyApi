2712703b1b227a9172b2cd92904de7993031eaa6
Mastersam <abadasamuelosp@gmail.com>
2020-06-13 17:21:34 -0700

    v0.9

--- polls/models.py
+++ polls/models.py
@@ -1,11 +1,14 @@
 import datetime
 
 from django.db import models
+from django.contrib.auth.models import User
 from django.utils import timezone
 
 
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
+class Poll(models.Model):
+    question = models.CharField(max_length=100)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    # pub_date = models.DateTimeField(auto_now=True)
     pub_date = models.DateTimeField('date published')
 
     def was_published_recently(self):
@@ -13,13 +16,21 @@ class Question(models.Model):
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
     def __str__(self):
-        return self.question_text
+        return self.question
 
 
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+    poll = models.ForeignKey(Poll, related_name='choices', on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=100)
 
     def __str__(self):
         return self.choice_text
+
+
+class Vote(models.Model):
+    choice = models.ForeignKey(Choice, related_name='votes', on_delete=models.CASCADE)
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
+    voted_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ("poll", "voted_by")
--- pollsApi/urls.py
+++ pollsApi/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from rest_framework_swagger.views import get_swagger_view
+from rest_framework.documentation import include_docs_urls
+
+schema_view = get_swagger_view(title='Polls API')
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('api/', include('polls.urls')),
+    path('master/', admin.site.urls),
+    path(r'swagger-docs/', schema_view),
+    path(r'docs/', include_docs_urls(title='Polls API')),
 ]
